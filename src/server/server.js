39bac9557e323de9f6de89f77c53ff3d85f11721
AnastasiaGritsuk <natikana_@mail.ru>
2016-05-24 13:32:34 +0300

    isMy(request.url)

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,21 @@
 var http = require('http');
 var ecstatic = require('ecstatic');
-var handler = ecstatic({ root: __dirname + '/public', handleError:false });
+var handler = ecstatic({ root: '../client', handleError:false });
 
 http.createServer(function(request, response) {
-	console.log('path is ' + __dirname);
-	console.log('handler ' + handler);
-
-	if(request.url == "/"){
-		handler.apply(null, arguments);
-	}else{
+	if(isMy(request.url)){
 		requestListener(request, response);
-	}
-	
+	}else{
+		handler.apply(null, arguments);
+	}	
+
 }).listen(8080);
 
 console.log('Listening on :8080');
 
-
+function isMy(url){
+	return url == '/chat' || url == "/history";
+}
 
 function send404Response(response){
 	response.writeHead(404, {"Content-Type":"text/plain"});
@@ -29,18 +28,16 @@ var arr = [];
 //Handle user requests
 function requestListener(request, response) {
 
-	if(request.method == "POST" && request.url == "/add"){
-
+	if(request.method == "POST" && request.url == "/chat"){
 		request.on('data', function(chunk) {
 			arr.push(chunk.toString());
 			response.write("added " + chunk);
 		}).on('end', function() {
 		  	response.end();
 		});
-	}else if(request.method == "GET" && request.url == "/getData"){
+	}else if(request.method == "GET" && request.url == "/history"){
 		response.writeHead(200, {"Content-Type":"text/plain"});
 		response.write(arr.toString());
 		response.end();
 	}
 }
-
